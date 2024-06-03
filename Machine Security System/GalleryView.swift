57a3c7ec23dcd208dda0671182ade9ae786f0aa4
oil3 <3oiltree@gmail.com>
2024-06-03 12:37:45 -0500

    before mergfe

--- Machine Security System/GalleryView.swift
+++ Machine Security System/GalleryView.swift
@@ -120,33 +120,4 @@ struct ImagePreviewView: View {
     }
 }
 
-struct VideoPreviewView: View {
-    var videoURL: URL
-    var onDismiss: () -> Void
 
-    var body: some View {
-        VStack {
-            HStack {
-                Button(action: onDismiss) {
-                    Text("Go Back")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                Spacer()
-            }
-            .padding()
-
-            Spacer()
-
-            VideoPlayer(player: AVPlayer(url: videoURL))
-                .frame(height: 300)
-
-            Spacer()
-        }
-        .background(Color.black.edgesIgnoringSafeArea(.all))
-        .transition(.move(edge: .bottom))
-        .animation(.easeInOut)
-    }
-}
